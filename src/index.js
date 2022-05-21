06a62e2fc084906362a37ae5b4b31bd02d04f122
Aleksandr-Sidorskiy <sanu43967@gmail.com>
2022-05-21 18:12:29 +0300

    problem

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-// import './css/styles.css';
+import './css/styles.css';
 // import { fetchCountries } from './js/fetchCountries';
 // import { countryСardTeemplate, countryListTemplate } from './js/markupTemplate';
 
-// const searchBox = document.querySelector('input#search-box');
-// const countryList = document.querySelector('.country-list');
-// const countryInfo = document.querySelector('country-info');
+const searchBox = document.querySelector('input#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('country-info');
 
-// console.log(searchBox);
+console.log(searchBox);
 
-// const DEBOUNCE_DELAY = 300;
+const DEBOUNCE_DELAY = 300;
 
 // searchBox.addEventListener('input' , 
 
-// fetch('https://restcountries.com/v3.1/name/ukraine')
-//     .then(responce => {
-//         return responce.json();
-//     })
-//     .then(name => {
-//         console.log(name);
-//     })
-//     .catch(error => {
-//         console.log(error);
-//     })
+fetch('https://restcountries.com/v3.1/name/ukraine')
+    .then(responce => {
+        return responce.json();
+    })
+    .then(name => {
+        console.log(name);
+    })
+    .catch(error => {
+        console.log(error);
+    })
